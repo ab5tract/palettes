bee7f04cc81f1c79beadf84af5e207d94e600d98
ab5tract <ethyrnet@yahoo.com>
2008-11-01 16:23:16 -0400

    moved back to format=xml because CL's json responses are broken. also, Palettes::ColourLovers::Palette is now just Palettes::ColourLovers.

--- lib/colourlovers.rb
+++ lib/colourlovers.rb
@@ -3,62 +3,65 @@ require 'rubygems'
 require 'mixin'
 
 module Palettes
-  module ColourLovers
+  class ColourLovers
     require 'httparty'
     
-    class Palette
-      include Palettes::Mixin
- 
-      def initialize(palette_id, names=false)
-        @palette = self.class.get_palette(palette_id, names)
-      end
- 
-      # Return the raw hash response provided by HTTParty
-      def response
-        @palette
-      end
-      
-      # CLASS METHODS!
-      include HTTParty
-    
-      base_uri 'www.colourlovers.com'
-      default_params :format => 'json'
-      format :json
-  
-      # Returns { :title => id }
-      def self.search(*keywords)
-        words = keywords.inject {|result,keywword| result += "+#{keyword}"} if keywords.respond_to? :last
-        words.gsub!(' ','+')
+    include Palettes::Mixin
 
-        palettes = get("/api/palettes", :query => { :keywords => words })
-        
-        titles = []; ids = []
-        palettes.each do |palette|
-          titles << palette['title']
-          ids << palette['id']
-        end
-        composites={}
-        (0..(titles.length-1)).each {|x| composites[titles[x]] = ids[x]}
-        composites
-      end
-    
-      def self.get_palette(palette_id, names=false)
-        #raise ArgumentError unless palette_id.is_a? Integer
-        @palette = get("/api/palette/#{palette_id.to_s}")
-        colors ||= @palette.pop['colors']
-        
-        if names
-          color_names=[]
-          colors.each do |c|
-            response = get("/api/color/#{c.to_s}")
-            color_names << response.pop['title']
-          end
-          colors.map! { |c| [color_names.shift, c] }
-        else
-          colors
-        end
-      end
-      
+    def initialize(palette_id, names=false)
+      @palette = self.class.get_palette(palette_id, names)
     end
+
+    # Return the raw hash response provided by HTTParty
+    def raw
+      @palette
+    end
+    
+    # CLASS METHODS!
+    include HTTParty
+  
+    base_uri 'www.colourlovers.com'
+#      default_params :format => 'json'
+#      format :json
+
+    default_params :format => 'xml'
+    format :xml
+
+    # Returns { :title => id }
+    def self.search(*keywords)
+      words = keywords.inject {|result,keywword| result += "+#{keyword}"} if keywords.respond_to? :last
+      words.gsub!(' ','+')
+
+      palettes = get("/api/palettes", :query => { :keywords => words })
+      palettes = palettes['palettes']['palette'] # This gets us the array of palette hashes
+      titles = []; ids = []; colors = []; creators = []
+      palettes.each do |palette|
+        titles << palette['title']
+        ids << palette['id']
+        colors << palette['colors']['hex']
+        creators << palette['userName']
+      end
+      composites={}
+      (0..(titles.length-1)).each {|x| composites[titles[x]] = {:id => ids[x], :colors => colors[x], :creator => creators[x]} }
+      composites
+    end
+  
+    def self.get_palette(palette_id, names=false)
+      #raise ArgumentError unless palette_id.is_a? Integer
+      @palette = get("/api/palette/#{palette_id.to_s}")
+      colors ||= @palette['palettes']['palette']['colors']['hex']
+      
+      if names
+        color_names=[]
+        colors.each do |c|
+          response = get("/api/color/#{c.to_s}")
+          color_names << response['colors']['color']['title']
+        end
+        colors.map! { |c| [color_names.shift, c] }
+      else
+        colors
+      end
+    end
+
   end
 end
